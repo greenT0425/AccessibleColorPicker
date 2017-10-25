8318b051278d129315ce887df34ad9ca8cd6cbd6
greenT0425 <tetsuro.recruit@gmail.com>
2017-10-25 23:08:02 +0900

    スクリーンショットの貼り付けテスト

--- app/scripts.babel/popup.js
+++ app/scripts.babel/popup.js
@@ -1,3 +1,10 @@
 'use strict';
 
 console.log('\'Allo \'Allo! Popup');
+console.log(chrome.extension.getViews());
+chrome.tabs.captureVisibleTab({format:"png"},function(screenshotUrl) {
+  setScreenshotUrl(screenshotUrl);
+});
+function setScreenshotUrl(url) {
+  document.getElementById('target').src = url;
+}
