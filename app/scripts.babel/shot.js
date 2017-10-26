b5dbfc5c2f5675f08976f12e545c049fc9e54829
Tetsuro Kishino <kishino@pepabo.com>
2017-10-26 20:29:45 +0900

    クリックしたら消えるように実装

--- app/scripts.babel/shot.js
+++ app/scripts.babel/shot.js
@@ -2,6 +2,7 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse){
   var screenshotImg = document.createElement('img');
   screenshotImg.id = 'sampleTarget';
   screenshotImg.src = request.url;
+  screenshotImg.addEventListener('click', () => document.body.removeChild(screenshotImg), false);
   document.body.appendChild(screenshotImg);
   return true;
 })
