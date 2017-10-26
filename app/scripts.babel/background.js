db19898a59de74018c7c486ed3686c5106097f41
Tetsuro Kishino <kishino@pepabo.com>
2017-10-26 20:15:02 +0900

    スタイルをcssに分離

--- app/scripts.babel/background.js
+++ app/scripts.babel/background.js
@@ -4,15 +4,12 @@ chrome.runtime.onInstalled.addListener(details => {
   console.log('previousVersion', details.previousVersion);
 });
 
-chrome.browserAction.setBadgeText({text: '\'Allo'});
-
-console.log('\'Allo \'Allo! Event Page for Browser Action');
-
 chrome.browserAction.onClicked.addListener(function() {
   chrome.tabs.query({ currentWindow: true, active: true },
     function (tabArray) {
       chrome.tabs.captureVisibleTab({format:'png'},function(screenshotUrl) {
         chrome.tabs.sendMessage(tabArray[0].id,{url:screenshotUrl});
       });
-    });
+    }
+  );
 });
--- app/scripts.babel/shot.js
+++ app/scripts.babel/shot.js
@@ -1,8 +1,6 @@
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse){
   var screenshotImg = document.createElement('img');
-  screenshotImg.style.position = 'fixed';
-  screenshotImg.style.top = '0';
-  screenshotImg.style.zIndex = '100';
+  screenshotImg.id = 'sampleTarget';
   screenshotImg.src = request.url;
   document.body.appendChild(screenshotImg);
   return true;
