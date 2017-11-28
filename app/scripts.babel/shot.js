0c1b834bd7a887079f77f82913a171cfed11269a
Tetsuro Kishino <kishino@pepabo.com>
2017-11-28 20:36:00 +0900

    Revert "canvasに画像挿入テスト"
    This reverts commit 68e296bfbc8d2d8c70d1b15b8453e98d298fb1f3.

--- app/scripts.babel/shot.js
+++ app/scripts.babel/shot.js
@@ -1,23 +1,8 @@
-
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse){
-  // var screenshotImg = document.createElement('img');
-  // screenshotImg.id = 'sampleTarget';
-  // screenshotImg.src = request.url;
-  draw(request.url);
-
-
-
-  // screenshotImg.addEventListener('click', () => document.body.removeChild(screenshotImg), false);
+  var screenshotImg = document.createElement('img');
+  screenshotImg.id = 'sampleTarget';
+  screenshotImg.src = request.url;
+  screenshotImg.addEventListener('click', () => document.body.removeChild(screenshotImg), false);
+  document.body.appendChild(screenshotImg);
   return true;
 })
-
-function draw(imgSrc) {
-  var canvas = document.createElement('canvas');
-  canvas.id = 'sampleTarget';
-  if ( ! canvas || ! canvas.getContext ) { return false; }
-  var ctx = canvas.getContext('2d');
-  var img = new Image();
-  img.src = imgSrc;
-  ctx.drawImage(img, 0, 0);
-  document.body.appendChild(canvas);
-}
